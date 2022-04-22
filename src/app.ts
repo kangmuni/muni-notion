fed3431fa39f94e7353e53280ff279afe75ceff1
kangmuni <kangmoonee@gmail.com>
2022-04-22 17:42:19 +0900

    Make imageComponent

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { ImageComponent } from './components/item/image.js';
 import { PageComponent } from './components/page/page.js';
 
 class App {
@@ -5,6 +6,12 @@ class App {
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      'ImageTitle',
+      'https://picsum.photos/600/300'
+    );
+    image.attachTo(appRoot, 'beforeend');
   }
 }
 
