fae50d181c31a5093fd32416b58a2ee7d9c6d27a
kangmuni <kangmoonee@gmail.com>
2022-04-25 15:14:06 +0900

    Make noteComponent and todoComponent

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import { ImageComponent } from './components/item/image.js';
+import { NoteComponent } from './components/item/note.js';
+import { TodoComponent } from './components/item/todo.js';
 import { PageComponent } from './components/page/page.js';
 
 class App {
@@ -12,6 +14,12 @@ class App {
       'https://picsum.photos/600/300'
     );
     image.attachTo(appRoot, 'beforeend');
+
+    const note = new NoteComponent('Note Title', 'Note Body');
+    note.attachTo(appRoot, 'beforeend');
+
+    const todo = new TodoComponent('Todo Title', 'Todo Item');
+    todo.attachTo(appRoot, 'beforeend');
   }
 }
 
