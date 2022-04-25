52bcfb61a0beee67484383f3180913b4a27fe200
kangmuni <kangmoonee@gmail.com>
2022-04-25 15:50:46 +0900

    Make videoComponent

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { ImageComponent } from './components/item/image.js';
 import { NoteComponent } from './components/item/note.js';
 import { TodoComponent } from './components/item/todo.js';
+import { VideoComponent } from './components/item/video.js';
 import { PageComponent } from './components/page/page.js';
 
 class App {
@@ -14,6 +15,12 @@ class App {
       'https://picsum.photos/600/300'
     );
     image.attachTo(appRoot, 'beforeend');
+
+    const video = new VideoComponent(
+      'Video Title',
+      'https://www.youtube.com/watch?v=TEXZSzPpZqo'
+    );
+    video.attachTo(appRoot, 'beforeend');
 
     const note = new NoteComponent('Note Title', 'Note Body');
     note.attachTo(appRoot, 'beforeend');
