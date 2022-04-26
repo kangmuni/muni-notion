ad4fe1c49e2471e8c38cc1a2492de4010cdbe212
kangmuni <kangmoonee@gmail.com>
2022-04-26 14:12:40 +0900

    Make pageItemComponent

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
+import { Component } from './components/component';
 import { ImageComponent } from './components/item/image.js';
 import { NoteComponent } from './components/item/note.js';
 import { TodoComponent } from './components/item/todo.js';
 import { VideoComponent } from './components/item/video.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
@@ -14,19 +15,19 @@ class App {
       'ImageTitle',
       'https://picsum.photos/600/300'
     );
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
 
     const video = new VideoComponent(
       'Video Title',
       'https://www.youtube.com/watch?v=TEXZSzPpZqo'
     );
-    video.attachTo(appRoot, 'beforeend');
+    this.page.addChild(video);
 
     const note = new NoteComponent('Note Title', 'Note Body');
-    note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
 
     const todo = new TodoComponent('Todo Title', 'Todo Item');
-    todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
   }
 }
 
