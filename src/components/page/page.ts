d03b8b8420f5d61db2f01777b97307c3d29ef3da
kangmuni <kangmoonee@gmail.com>
2022-04-27 09:30:25 +0900

    Make dependency-injection in pageComponent

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -6,9 +6,17 @@ export interface Composable {
 
 type OncloseListener = () => void;
 
+interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OncloseListener): void;
+}
+
+type SectionContainerConstructor = {
+  new (): SectionContainer;
+};
+
 export class PageItemComponent
   extends BaseComponent<HTMLElement>
-  implements Composable
+  implements SectionContainer
 {
   private closeListener?: OncloseListener;
 
@@ -40,12 +48,12 @@ export class PageComponent
   extends BaseComponent<HTMLUListElement>
   implements Composable
 {
-  constructor() {
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
   }
 
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
     item.setOnCloseListener(() => {
